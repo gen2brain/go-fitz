7ce80f7fc8f60f575886046c55954b0e6078a62b
Milan Nikolic <gen2brain@gmail.com>
2021-05-24 11:37:21 +0200

    Test compat

--- fitz_cgo_extlib.go
+++ fitz_cgo_extlib.go
@@ -4,7 +4,8 @@ package fitz
 
 /*
 #cgo !static LDFLAGS: -lmupdf -lm
-#cgo static LDFLAGS: -lmupdf -lm -lmupdf-third
+#cgo static,!compat LDFLAGS: -lmupdf -lm -lmupdf-third
+#cgo static,compat LDFLAGS: -lmupdf -lm -lmupdfthird
 #cgo android LDFLAGS: -llog
 #cgo windows LDFLAGS: -lcomdlg32 -lgdi32
 */
