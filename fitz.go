3f63a564725bac7ad8957f26a675e916d22a882b
Milan Nikolic <gen2brain@gmail.com>
2023-08-24 11:12:44 +0200

    Remove check

--- fitz.go
+++ fitz.go
@@ -101,18 +101,6 @@ func New(filename string) (f *Document, err error) {
 		return
 	}
 
-	b, e := os.ReadFile(filename)
-	if e != nil {
-		err = ErrOpenDocument
-		return
-	}
-
-	magic := contentType(b)
-	if magic == "" {
-		err = ErrOpenDocument
-		return
-	}
-
 	f.ctx = (*C.struct_fz_context)(unsafe.Pointer(C.fz_new_context_imp(nil, nil, C.FZ_STORE_UNLIMITED, C.fz_version)))
 	if f.ctx == nil {
 		err = ErrCreateContext
